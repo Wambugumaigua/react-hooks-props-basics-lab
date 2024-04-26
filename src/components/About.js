59db930323c90ce76ddb0eff49521bf775adbc68
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-04-26 02:05:02 -0700

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
+import user from "../data/user";
 
-function About() {
+function About(props) {
+  const { github, linkedin } = user.links;
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
     </div>
   );
 }
